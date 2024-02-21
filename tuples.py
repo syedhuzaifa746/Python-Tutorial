bd4191c3be4d5c7a6c8d732ca659442ba58a43e6
syedhuzaifa746 <107565690+syedhuzaifa746@users.noreply.github.com>
2024-02-21 11:42:13 +0500

    Tuples in Python
    Tuples in Python with simple test cases

---
+++ tuples.py
@@ -0,0 +1,20 @@
+# tuples which is ordered and unchangable used to group togther related data
+
+student = ("Huzaifa", 23, "male")
+
+# print(student.count("Huzaifa"))
+# print(student.index("male"))
+
+# for x in range (len(student)):
+#     print(student[x])
+
+input = int(input("Enter any number"))
+
+# Iterating over a range
+for num in range(input):
+    if(num  % 2 == 0):
+        print(f"{num} is even")
+    else:
+        print(f"{num} is odd")
+    
+
