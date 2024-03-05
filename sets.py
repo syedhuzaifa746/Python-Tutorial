ff92dde9929c053d5103ac4df9c7f950fc5b54d4
Syed Huzaifa <107565690+syedhuzaifa746@users.noreply.github.com>
2024-03-05 11:10:35 +0500

    Create sets.py

---
+++ sets.py
@@ -0,0 +1,18 @@
+# set = collection which is unordered, unindexed. No duplicate values.
+
+utensils = {"forks", "spoon", "knife", "knife"}
+dishes = {"ball", "plate", "cup", "knife"}
+
+# utensils.add("napkin")
+# utensils.remove("fork")
+# utensils.clear()
+
+# utensils.update(dishes)
+# dinner_table = utensils.union(dishes)
+
+# print(utensils.difference(dishes))
+
+print(utensils.intersection(dishes))
+
+# for x in dinner_table:
+#     print(x)
